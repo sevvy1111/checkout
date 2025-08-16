85c498d5b470753c21a27f430f46741d28a38843
sevvy1111 <cmguevarra@student.hau.edu.ph>
2025-08-17 05:21:31 +0800

    dont know

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
+# checkout/start.sh
+
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+daphne -b 0.0.0.0 -p $PORT marketplace.asgi:application
