337610ea93b861b335826d5493ff783c5bd30ecc
Sev <cmguevarra@student.hau.edu.ph>
2025-08-16 20:40:59 +0800

    feat: Add CSRF_TRUSTED_ORIGINS to settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -12,6 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cym2oo+i$d*-9aj=eazma
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['checkoutph.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://checkoutph.onrender.com']
 
 INSTALLED_APPS = [
     'daphne',
