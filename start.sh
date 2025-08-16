9f107e8e47926b772d063ae5acc728ff2eb0ec88
sevvy1111 <cmguevarra@student.hau.edu.ph>
2025-08-16 19:56:30 +0800

    deployment test 3

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Exit on first error
+set -e
+
+# Run database migrations
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start the Daphne ASGI server
+echo "Starting Daphne server..."
+DJANGO_SETTINGS_MODULE=marketplace.settings daphne -b 0.0.0.0 -p 8000 marketplace.asgi:application
