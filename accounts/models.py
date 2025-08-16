ba654e59039c9b7a066059f10a8d1f04809feedd
sevvy1111 <cmguevarra@student.hau.edu.ph>
2025-08-17 04:38:47 +0800

    fix(media): Resolve Cloudinary URL conflict in Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="avatars/", default="avatar.svg", blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
     phone = models.CharField(
         max_length=13,
         null=True,
