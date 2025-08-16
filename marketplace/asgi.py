66867a3bb20a40285af6c590401f8faeacf32e98
Sev <cmguevarra@student.hau.edu.ph>
2025-08-16 16:47:42 +0800

    fix(asgi): Resolve NameError by correctly importing messaging

--- marketplace/asgi.py
+++ marketplace/asgi.py
@@ -9,13 +9,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
 # The `get_asgi_application()` call is essential for loading Django's app registry
 django_asgi_app = get_asgi_application()
 
+import messaging.routing # Correct import location
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            # This import needs to be inside the URLRouter to be executed after the app registry is ready.
-            # Make sure you remove the import from the top of the file.
-            (lambda: messaging.routing.websocket_urlpatterns)()
+            messaging.routing.websocket_urlpatterns
         )
     ),
 })
