38ca50fec326da5424789ffb3de6c613cd9ec1d4
sevvy1111 <cmguevarra@student.hau.edu.ph>
2025-08-18 06:17:52 +0800

    feat: notif read/unread

--- notifications/urls.py
+++ notifications/urls.py
@@ -7,4 +7,5 @@ app_name = 'notifications'
 urlpatterns = [
     path('', views.NotificationListView.as_view(), name='all'),
     path('mark-as-read/<int:notification_id>/', views.mark_as_read, name='mark_as_read'),
+    path('read/<int:notification_id>/', views.redirect_and_mark_as_read, name='read_and_redirect'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -1,8 +1,9 @@
-# notifications/views.py
+# sevvy1111/checkout/checkout-6284e1df24802e516d66595b54136462676a4c2c/notifications/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Notification
 from django.views.generic import ListView
+from django.http import JsonResponse
+from .models import Notification
 
 
 class NotificationListView(ListView):
@@ -12,6 +13,9 @@ class NotificationListView(ListView):
     paginate_by = 20
 
     def get_queryset(self):
+        # First, mark all unread notifications as read when visiting this page
+        Notification.objects.filter(recipient=self.request.user, is_read=False).update(is_read=True)
+        # Then, return all notifications for the user
         return Notification.objects.filter(recipient=self.request.user).order_by('-timestamp')
 
 
@@ -20,4 +24,21 @@ def mark_as_read(request, notification_id):
     notification = Notification.objects.get(id=notification_id, recipient=request.user)
     notification.is_read = True
     notification.save()
+    return redirect('notifications:all')
+
+
+@login_required
+def redirect_and_mark_as_read(request, notification_id):
+    """
+    Marks a specific notification as read and then redirects the user
+    to the notification's link.
+    """
+    notification = get_object_or_404(Notification, id=notification_id, recipient=request.user)
+    notification.is_read = True
+    notification.save()
+
+    if notification.link:
+        return redirect(notification.link)
+
+    # Fallback in case there is no link
     return redirect('notifications:all')
